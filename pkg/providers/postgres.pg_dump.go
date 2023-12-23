5c6877764a990e96fd5ece753a79fd4ad8658f0f
Stanislav <stanislav.s@itsyndicate.org>
2023-12-23 11:01:43 +0200

    added providers package

--- pkg/providers/postgres.pg_dump.go
+++ pkg/providers/postgres.pg_dump.go
@@ -3,15 +3,25 @@ package providers
 import (
 	"github.com/habx/pg-commands"
 	"github.com/pibblokto/backlokto/pkg/types"
+	"os"
 )
 
 func PostgresPgDump(job types.BackupJob) {
+
+	var pgPass string
+
+	if job.Spec.Password == nil {
+		pgPass = os.Getenv("PG_PASS")
+	} else {
+		pgPass = job.Spec.Password
+	}
+
 	dump, _ := pg.NewDump(&pg.Postgres{
-		Host:     job.Spec.,
-		Port:     5432,
-		DB:       "dev_example",
-		Username: "example",
-		Password: "example",
+		Host:     job.Spec.Host,
+		Port:     job.Spec.Port,
+		DB:       job.Spec.DB,
+		Username: job.Spec.Username,
+		Password: pgPass,
 	})
 
 	dumpExec := dump.Exec(pg.ExecOptions{StreamPrint: false})
